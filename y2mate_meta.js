174a2f872787ba2a4f15b7639802008b0bc9f18b
joselmm <95531879+joselmm@users.noreply.github.com>
2023-01-17 13:13:53 +0000

    Create y2mate_meta.js
    primera edición del archivo con los metadatos

---
+++ y2mate_meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Descargar MP3 de y2
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.youtube.com/results?search_query=*
+// @match        https://www.y2mate.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @updateURL    http://circulodorado.onlinewebshop.net/extensions/chrome/y2mate.js
+// @downloadURL  http://circulodorado.onlinewebshop.net/extensions/chrome/y2mate_meta.js
+// @grant        none
+// ==/UserScript==
