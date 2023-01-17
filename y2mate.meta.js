c0e1368504e790691a6f43edadffc87f6e2ccb53
joselmm <95531879+joselmm@users.noreply.github.com>
2023-01-17 19:24:28 +0000

    Update y2mate.meta.js

--- y2mate.meta.js
+++ y2mate.meta.js
@@ -4,7 +4,7 @@
 // @version      1.0
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.youtube.com/results?search_query=*
+// @match        https://www.youtube.com/*
 // @match        https://www.y2mate.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @downloadURL  https://raw.githubusercontent.com/joselmm/y2mate/main/y2mate.user.js
