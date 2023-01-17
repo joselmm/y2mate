7eea39cadd2a62b05a444af56c2e3b7369b30550
joselmm <95531879+joselmm@users.noreply.github.com>
2023-01-17 13:32:42 +0000

    Update y2mate.meta.js

--- y2mate.meta.js
+++ y2mate.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Descargar MP3 de y2
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.youtube.com/results?search_query=*
