734273f54d4b87947a7890ad060baf061e90a9c3
joselmm <95531879+joselmm@users.noreply.github.com>
2023-02-17 12:31:17 -0500

    Update y2mate.meta.js

--- y2mate.meta.js
+++ y2mate.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Y2 Mate decargas
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  Descargar mp3 de Youtube 
 // @author       joselmm
 // @match        https://www.youtube.com/*
