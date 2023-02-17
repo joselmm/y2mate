cd5d28bc022954e04ed10c4e00ee33d7d2304c1b
joselmm <95531879+joselmm@users.noreply.github.com>
2023-02-17 12:29:57 -0500

    Update y2mate.user.js

--- y2mate.user.js
+++ y2mate.user.js
@@ -25,9 +25,6 @@
             if(document.querySelector('.btn.btn-success.btn-file')){document.querySelector('.btn.btn-success.btn-file').click(); clearInterval(btnSuccessInt)}
         }
         btnSuccessInt=setInterval(btnSuccessFunc, 300)
-
-
-
         return
     }
 
@@ -54,12 +51,12 @@
     setInterval(app, 500)
 
     function act() {
-if(document.querySelectorAll('.matey2')){document.querySelectorAll('.matey2').forEach((ele)=>{ele.outerHTML=''})
-}
+        if(document.querySelectorAll('.matey2')){document.querySelectorAll('.matey2').forEach((ele)=>{ele.outerHTML=''})
+                                                }
 
 
         var height = document.querySelector("#channel-thumbnail").clientHeight
-        var width  = 3*height
+        var width = 3*height
 
         var links = document.querySelectorAll('div ytd-video-renderer ytd-thumbnail a');
         var channelsBoxes = document.querySelectorAll('#channel-info')
@@ -69,7 +66,7 @@ if(document.querySelectorAll('.matey2')){document.querySelectorAll('.matey2').fo
             redBox.classList.add('matey2')
             redBox.style.width = width+'px';
             redBox.style.height = height+'px';
-           // redBox.style.backgroundColor = 'red';
+            // redBox.style.backgroundColor = 'red';
             redBox.style.borderRadius = '5px';
             channelsBoxes[i].appendChild(redBox);
             var link = document.createElement('a');
@@ -82,4 +79,61 @@ if(document.querySelectorAll('.matey2')){document.querySelectorAll('.matey2').fo
 
         }
     }
+
+
+
+    var vId=extraerIdDeVideo(location.href);
+    function titleFunc(){
+        if(!location.href.includes("https://www.youtube.com/watch")){return}
+        if(document.querySelector(".style-scope.ytd-watch-metadata .style-scope.ytd-watch-metadata h1.style-scope.ytd-watch-metadata")){ejecutar();}
+    }
+    var btnInt=setInterval(titleFunc, 300);
+    function ejecutar(){
+
+        if(document.querySelector("#download-watch")){document.querySelector("#download-watch").outerHTML=""}
+
+        var titleBoxParent = document.querySelector(".style-scope.ytd-watch-metadata .style-scope.ytd-watch-metadata h1.style-scope.ytd-watch-metadata");
+        //creacion del boton de descaragar abajao del video
+        var redBox = document.createElement('div');
+        titleBoxParent.appendChild(redBox);
+        redBox.outerHTML=`<div id="download-watch" style="border-radius: 5px;display: inline-block;right: 0;position: absolute;">
+            <a target="_blank" id="current-video-link">Descargar</a>
+        </div>`
+        var aTag= document.querySelector("#current-video-link");
+        aTag.href="https://www.y2mate.com/youtube-mp3/"+extraerIdDeVideo(location.href);
+    }
+
+
+
+    function extraerIdDeVideo(url) {
+        const expresion = /^(?:https?:\/\/)?(?:www\.)?(?:m\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=))([\w-]{11})(?:\S+)?$/;
+        const match = url.match(expresion);
+        return match ? match[1] : null;
+    }
+
+
+
+
+
+    function recomendados(){
+        if(!location.href.includes("https://www.youtube.com/watch")){return}
+        if(document.querySelectorAll("#dismissible > div > div.metadata.style-scope.ytd-compact-video-renderer > a").length){ejecutarListaReco();}
+    }
+    var listInt=setInterval(recomendados, 300);
+
+    function ejecutarListaReco() {
+        var list=document.querySelectorAll("#dismissible > div > div.metadata.style-scope.ytd-compact-video-renderer > a");
+        if(document.querySelectorAll(".list-a").length){document.querySelectorAll(".list-a").forEach((div)=>{div.outerHTML=""})}
+        for(let a of list){
+
+            var redBox = document.createElement('div');
+            a.parentNode.parentNode.appendChild(redBox);
+            redBox.outerHTML=`<div class="list-a" style="border-radius: 5px;right: 0;position: absolute;font-size:${(a.parentNode.parentNode.parentNode.clientHeight/6)+"px"}">
+            <a href="https://www.y2mate.com/youtube-mp3/${extraerIdDeVideo(a.href)}" target="_blank" id="current-video-link">Descargar</a>
+        </div>`
+
+        }
+    }
+
+
 })();
